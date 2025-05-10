ba21a681a8c1e3399ac080ac0970f0c3ae5f924f
miloszjucha <miloszjucha3@gmail.com>
2025-05-10 20:06:08 +0200

    Update instruction.py
    Created instruction function - selection of sudoku types instruction.
    Created 5 stub functions

--- instruction.py
+++ instruction.py
@@ -1,2 +1,24 @@
 def clear_terminal():
-  print("\033[2J\033[H", end=""]
+  print("\033c", end="")
+def instruction ():
+  print("Rules of which sudoku would you like to see:")
+  print("1. Classic sudoku")
+  print("2. Diagonal sudoku")
+  print("3. Region sudoku")
+  print("4. Killer sudoku")
+  print("5. Jigsaw sudoku")
+
+def classic_sudoku ():
+  pass #TODO
+  
+def diagonal_sudoku ():
+  pass #TODO
+  
+def region_sudoku ():
+  pass #TODO
+  
+def killer_sudoku ():
+  pass #TODO
+  
+def jigsaw_sudoku ():
+  pass #TODO
