02a266521477a76830e8f30fd2f285a1d62c6c2b
dheczko <daniel.heczko20@gmail.com>
2025-05-13 21:18:37 +0200

    Print colored game layout

--- game.py
+++ game.py
@@ -1,9 +1,15 @@
 # from math import ceil
+from graphic import change_text_color
 
 def game():
     rows_cols_no = 9
     cell_width = 3
+    group_by = 3
 
+    default_color = "white"
+    fulfillment_color = "cyan"
+
+    change_text_color(default_color)
     print("╔", end="")
     for _ in range(rows_cols_no-1):
         print("═" * cell_width + "╦", end="")
@@ -15,13 +21,19 @@ def game():
                 # if n == cell_width // 2 // 2:
                 #     print("║" + " " * ((cell_width-1) // 2) + str((i*rows_cols_no+j)%10) + " " * ceil((cell_width-1) / 2), end="")
                 #     continue
+                change_text_color(fulfillment_color) if j % group_by != 0 else None
                 print("║" + " " * cell_width, end="")
+                change_text_color(default_color)
             print("║")
 
         if i != rows_cols_no - 1:
             print("╠", end="")
-            for _ in range(rows_cols_no-1):
-                print("═" * cell_width + "╬", end="")
+            for n in range(rows_cols_no-1):
+                change_text_color(fulfillment_color) if i % group_by != group_by - 1 else None
+                print("═" * cell_width, end="")
+                change_text_color(default_color) if n % group_by == group_by - 1 else None
+                print("╬", end="")
+                change_text_color(default_color)
             print("═" * cell_width + "╣")
         else:
             print("╚", end="")
