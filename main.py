b6ad6e04712b7e54a0b468eadeece5d33c762d0a
emiloosek <kaczynskie@outlook.com>
2025-05-14 19:35:08 +0200

    main_menu_fixed

--- main.py
+++ main.py
@@ -10,45 +10,53 @@ def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def print_mainmenu() -> int:
-    print("Gra, która pobudzi Twój umysł!")
-    sleep(1)
-    clear()
-    print("SUDOKU \n \n \n")
-    print("1. Zagraj")
-    print("2. Jak grać?")
-    print("3. Ustawienia")
-    print("4. Zakończ")
-    
-    choice = int(input("Wybierz opcję: "))
-    
-    match choice:
-        #game start
-        case 1:
-            clear()
-            game.game()
-            return 1
-        
-        #how to play    
-        case 2:
-            clear()
-            # instruction.instruction()
-            return 1
-        
-        #settings    
-        case 3:
-            #settings.settings()
-            clear()
-            return 1
-        
-        #game exit
-        case 4: 
-            clear()
-            sys.exit()   
-            return 1
-        
-        case _:
-            clear()
-            return 0
+    try:
+        print("Gra, która pobudzi Twój umysł!")
+        sleep(1)
+        clear()
+        print("SUDOKU \n \n \n")
+        print("1. Zagraj")
+        print("2. Jak grać?")
+        print("3. Ustawienia")
+        print("4. Zakończ")
+        choice = int(input("Wybierz opcję: "))
+        match choice:
+            #game start
+            case 1:
+                clear()
+                game.game()
+                return 1
+            
+            #how to play    
+            case 2:
+                clear()
+                # instruction.instruction()
+                return 1
+            
+            #settings    
+            case 3:
+                #settings.settings()
+                clear()
+                return 1
+            
+            #game exit
+            case 4: 
+                clear()
+                print("Dziękujemy za grę!")
+                sleep(1)
+                sys.exit()   
+                return 1
+            
+            case _:
+                print("Niepoprawny wybór, spróbuj ponownie.")
+                sleep(0.5)
+                clear()
+                
+    except ValueError:
+        print("Niepoprawny wybór, spróbuj ponownie.")
+        sleep(0.5)
+        clear()
+        return 0
         
 def main():
     while True:
@@ -59,5 +67,3 @@ def main():
             
 if __name__ == "__main__":
     main()
-        
-        
