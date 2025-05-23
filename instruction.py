39e361330cb2269289f20e8c2b0e38d86a178084
miloszjucha <miloszjucha3@gmail.com>
2025-05-23 20:02:40 +0200

    Update instruction.py
    finished implementing lm

--- instruction.py
+++ instruction.py
@@ -42,16 +42,18 @@ def instruction() -> None:
     while True:
         clear_terminal()
         print(lm.get("instruction.ChoseSudoku"))
-        print("1. Classic sudoku")
-        print("2. Diagonal sudoku")
-        print("3. Jigsaw sudoku")
-        print("4. Killer sudoku")
+        print(lm.get("instruction.ClassicSudoku"))
+        print(lm.get("instruction.DiagonalSudoku"))
+        print(lm.get("instruction.JigsawSudoku"))
+        print(lm.get("instruction.KillerSudoku"))
+        print("5. " + lm.get("instruction.GoBackToMainMenuOption"))
 
         options: dict[int, Callable[[], None]] = {
             1: classic_sudoku,
             2: diagonal_sudoku,
             3: jigsaw_sudoku,
-            4: killer_sudoku
+            4: killer_sudoku,
+            5: go_back_to_main_menu
         }
 
         try:
@@ -69,46 +71,46 @@ def instruction() -> None:
 
 def classic_sudoku () -> None:
   clear_terminal()
-  print("A standard Sudoku puzzle consists of a grid of 9 blocks. Each block contains 9 boxes arranged in 3 rows and 3 columns.\n")
-  print("Some blocks will be pre-filled for you and they cannot be changed.")
-  print("In order to solve the puzzle all 81 boxes must contain numbers and the Sudoku rules must be followed:\n") 
-  print("1. Each column must contain all digits from 1 to 9, with no repetitions.")
-  print("2. Each row must contain all digits from 1 to 9, with no repetitions.")
-  print("3. Each block must contain all digits from 1 to 9, with no repetitions.\n")
+  print(lm.get("instruction.ClassicIntro"))
+  print(lm.get("instruction.ClassicDetails1"))
+  print(lm.get("instruction.ClassicDetails2")) 
+  print(lm.get("instruction.ClassicRule1"))
+  print(lm.get("instruction.ClassicRule2"))
+  print(lm.get("instruction.ClassicRule3"))
   sleep(2)
   go_back_to_instruction()
   
 def diagonal_sudoku () -> None:
   clear_terminal()
-  print("A Diagonal Sudoku puzzle follows the standard rules of Sudoku with one change.\n")
-  print("Standard sudoku rules:")
-  print("1. Each column must contain all digits from 1 to 9, with no repetitions.")
-  print("2. Each row must contain all digits from 1 to 9, with no repetitions.")
-  print("3. Each block must contain all digits from 1 to 9, with no repetitions.")
-  print("Additionally both main diagonals must also contain all digits from 1 to 9, with no repetitions.\n")
+  print(lm.get("instruction.DiagonalIntro"))
+  print(lm.get("instruction.DiagonalStandardRules"))
+  print(lm.get("instruction.ClassicRule1"))
+  print(lm.get("instruction.ClassicRule2"))
+  print(lm.get("instruction.ClassicRule3"))
+  print(lm.get("instruction.DiagonalExtra"))
   sleep(2)
   go_back_to_instruction()
   
 def jigsaw_sudoku () -> None:
   clear_terminal()
-  print("A Jigsaw Sudoku puzzle is a variant of standard Sudoku where the 9 standard 3x3 boxes are replaced by 9 irregularly shaped regions.\n")
-  print("The puzzle follows the standard rules of Sudoku with one change:")
-  print("1. Each column must contain all digits from 1 to 9, with no repetitions.")
-  print("2. Each row must contain all digits from 1 to 9, with no repetitions.")
-  print("3. Each irregular region must contain all digits from 1 to 9, with no repetitions.\n")
+  print(lm.get("instruction.JigsawIntro"))
+  print(lm.get("instruction.JigsawChange"))
+  print(lm.get("instruction.ClassicRule1"))
+  print(lm.get("instruction.ClassicRule2"))
+  print(lm.get("instruction.JigsawRule3"))
   sleep(2)
   go_back_to_instruction()
   
   
 def killer_sudoku () -> None:
   clear_terminal()
-  print("A Killer Sudoku puzzle follows the standard rules of Sudoku with additional rules:\n")
-  print("1. Each column must contain all digits from 1 to 9, with no repetitions.")
-  print("2. Each row must contain all digits from 1 to 9, with no repetitions.")
-  print("3. Each block must contain all digits from 1 to 9, with no repetitions.")
-  print("Additionally the grid is divided into cages, outlined with dotted or bold lines")
-  print("The number shown in the upper corner of the cage is the target sum that the digits within that cage must add up to")
-  print("In each cage digits cannot be repeated\n")
+  print(lm.get("instruction.KillerIntro"))
+  print(lm.get("instruction.ClassicRule1"))
+  print(lm.get("instruction.ClassicRule2"))
+  print(lm.get("instruction.ClassicRule3"))
+  print(lm.get("instruction.KillerCage1"))
+  print(lm.get("instruction.KillerCage2"))
+  print(lm.get("instruction.KillerCage3"))
   sleep(2)
   go_back_to_instruction()
   
